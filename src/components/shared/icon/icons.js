daa78b65c20217a80f33efbe82c59f81b4f5a8b8
Brunno Morokuma <BrunnoMorokuma1234@Gmail.com>
2022-06-23 18:21:55 -0300

    Doing a ToDo List entire in JS

---
+++ src/components/shared/icon/icons.js
@@ -0,0 +1,24 @@
+export const ICONS = {
+  search: `
+  <svg xmlns="http://www.w3.org/2000/svg" width="17.012" height="17.012" viewBox="0 0 17.012 17.012">
+    <g id="Icon_feather-search" data-name="Icon feather-search" transform="translate(0.833 0.833)">
+      <path id="Caminho_1" data-name="Caminho 1" d="M15.833,9.167A6.667,6.667,0,1,1,9.167,2.5a6.667,6.667,0,0,1,6.667,6.667Z" transform="translate(-2.5 -2.5)" fill="none" stroke="#909eb2" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.667"/>
+     <path id="Caminho_2" data-name="Caminho 2" d="M17.5,17.5l-3.625-3.625" transform="translate(-2.5 -2.5)" fill="none" stroke="#909eb2" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.667"/>
+    </g>
+  </svg>
+`,
+  plus: `
+  <svg xmlns="http://www.w3.org/2000/svg" width="13.333" height="13.333" viewBox="0 0 13.333 13.333">
+    <g id="Icon_feather-plus" data-name="Icon feather-plus" transform="translate(0.833 0.833)">
+      <path id="Caminho_85" data-name="Caminho 85" d="M10,4.167V15.833" transform="translate(-4.167 -4.167)" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.667"/>
+      <path id="Caminho_86" data-name="Caminho 86" d="M4.167,10H15.833" transform="translate(-4.167 -4.167)" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.667"/>
+    </g>
+  </svg>
+`,
+  edit: `
+  <svg xmlns="http://www.w3.org/2000/svg" width="18.19" height="18.384" viewBox="0 0 18.19 18.384">
+    <path id="Icon_feather-edit-2" data-name="Icon feather-edit-2" d="M14.167,2.5A2.357,2.357,0,1,1,17.5,5.833L6.25,17.083l-4.583,1.25,1.25-4.583Z" transform="translate(-0.833 -0.783)" fill="none" stroke="#909eb2" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.667"/>
+  </svg>
+`,
+
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import { Container } from './components/Container/index.js  '
+import { Container } from './components/Container/index.js'
+import { Input } from './components/shared/Input/index.js'
+import { Icon } from './components/shared/icon/index.js'
 
 const $root = document.querySelector('#root')
 
-const container = Container({children: 'Plínio'})
+const icon = Icon('plus')
+
+const input = Input({
+  icon: 'search',
+  placeholder: 'Digite sua pesquisa aqui...',
+  width: '500px'
+})
+
+const container = Container({children: input })
 
 $root.appendChild(container)
